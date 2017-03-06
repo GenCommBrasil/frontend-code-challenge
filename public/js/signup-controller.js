2d48c3bb5a0de022a64b94f1e671864f773c4df8
daniel <ssales.daniel@gmail.com>
2017-03-06 18:23:31 -0300

    http post funcionando com required nos campos

--- public/js/signup-controller.js
+++ public/js/signup-controller.js
@@ -1,7 +1,11 @@
 app.controller("signupCtrl", function($scope, $http, mainService) {
-  
-    $scope.register = function () {
+
+    $scope.register = function() {
         console.log($scope.data);
+        $http.post('http://localhost:3000/users', $scope.data)
+            .then(function(data) {
+                console.log(data);
+            })
     }
-  
+
 });
