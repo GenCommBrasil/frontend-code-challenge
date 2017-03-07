da92b97f488d1096d22b4a74e6c5ee96fef80153
daniel <ssales.daniel@gmail.com>
2017-03-07 10:49:09 -0300

    exercicio pronto e organizado

--- public/js/controllers/signup-controller.js
+++ public/js/controllers/signup-controller.js
@@ -2,7 +2,10 @@ app.controller("signupCtrl", function($scope, $http, mainService) {
 
     $scope.register = function() {
         console.log($scope.data);
-        mainService.postData($scope.data);
+        mainService.postData($scope.data).then(function(){
+        	alert('Registered successfully');
+        	$scope.data = {};
+        });
     }
 
 });
--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-app.controller("regCtrl", function($scope, $http, mainService) {
-
-     mainService.getData().then(function(response){
-         $scope.users = response;
-         console.log($scope.users);
-    });
-
-});
